859520f5811feecf9611a0435e3ece5b8a56f56e
Nursena  Karakulah <senafrakara@gmail.com>
2025-05-08 12:57:47 +0300

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increment">Click me</button>
+    <p>You've clicked {{ count }} times.</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const title = ref('Hello Vue 3 with Composition API!');
+    const count = ref(0);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    return {
+      title,
+      count,
+      increment
+    };
+  }
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #42b983;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+button:hover {
+  background-color: #369870;
+}
+</style>
